82ec13fccb3eb1521a18002a3582df0f4902a9c8
Aravind KR <kraravind1993@gmail.com>
2020-08-09 11:11:19 +0530

    Project stub setup

---
+++ shoppingbackend/src/main/java/com/aravind/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package com.aravind.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
