e5ca4a0dd1aa81c5f10953b29f22824a40c3f433
Aravind KR <kraravind1993@gmail.com>
2020-08-11 10:27:12 +0530

    Completed the interface theme and other issues

---
+++ onlineshopping/src/main/webapp/WEB-INF/views/about.jsp
@@ -0,0 +1,7 @@
+<div class="container">
+	<div class="row">
+		<h1>About Us</h1>
+		<hr/>
+		<p>This is an about us page, come back later </p>
+	</div>
+</div>
---
+++ onlineshopping/src/main/webapp/assets/js/myapp.js
@@ -0,0 +1,14 @@
+$(function() {
+	//Solving active menu problem
+	switch(menu){
+	case 'About Us':
+		$('#about').addclass('active');
+		break;
+	case 'Contact Us':
+		$('#contact').addclass('active');
+		break;
+	default:
+		$('#home').addclass('active');
+	break;
+	}
+});
